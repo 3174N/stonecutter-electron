8f275d62f90b73031878f3d801aa2693daccc2bb
Embersands game Studios <eitan.levy.159@gmail.com>
2020-12-07 21:57:51 +0200

    Add init files

---
+++ index.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow } = require('electron');
+const url = require('url');
+const path = require('path');
+
+let win;
+
+function createWindow() {
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    });
+    win.loadURL(
+        url.format({
+            pathname: path.join(__dirname, 'index.html'),
+            protocol: 'file:',
+            slashes: true,
+        })
+    );
+}
+
+app.on('ready', createWindow);
