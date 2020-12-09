7602606813e94bdf84b2a44f02f19eb74a31a521
Embersands game Studios <eitan.levy.159@gmail.com>
2020-12-09 09:43:48 +0200

    Add reading files

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,30 @@
 const fs = require('fs');
+const $ = require('jquery');
+
+const { dialog } = require('electron').remote;
+
+document
+    .querySelector('#openfileBtn')
+    .addEventListener('click', function (event) {
+        dialog
+            .showOpenDialog({ properties: ['openFile'] })
+            .then(function (response) {
+                if (!response.canceled) {
+                    console.log('file: ' + response.filePaths[0]);
+                    fs.readFile(response.filePaths[0], function (err, data) {
+                        if (err) return console.log(err);
+
+                        $('.file-content').text('');
+
+                        const lines = data.toString().split('\n');
+
+                        for (let i = 0; i < lines.length; i++) {
+                            $('.file-content').append(lines[i] + '<br>');
+                            console.log(lines[i]);
+                        }
+                    });
+                } else {
+                    console.log('no file selected');
+                }
+            });
+    });
