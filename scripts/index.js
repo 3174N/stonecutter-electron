b4470977c03156d19af3b2c25d4db53ba1c1d8b5
Ethan Levy <eitan.levy.159@gmail.com>
2020-12-13 09:29:41 +0200

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -7,10 +7,10 @@ const { dialog } = require('electron').remote;
 
 // Used to get text with in-line breaks
 function parseBrakes(value) {
-    value = value.replace(/<div>/gi, '\n');
-    value = value.replace(/<(.*?)>/g, '');
+	value = value.replace(/<div>/gi, '\n');
+	value = value.replace(/<(.*?)>/g, '');
 
-    return value;
+	return value;
 }
 
 var filePaths = {};
@@ -18,151 +18,151 @@ var currentFile = '';
 
 // Open file
 document
-    .querySelector('#openfileBtn')
-    .addEventListener('click', function (event) {
-        dialog
-            .showOpenDialog({
-                title: 'Select a File to Open',
-                filters: [
-                    {
-                        name: 'Minecraft Function',
-                        extensions: ['mcfunction'],
-                    },
-                    {
-                        name: 'JavaScript Object Notation',
-                        extensions: ['json'],
-                    },
-                    {
-                        name: 'Text',
-                        extensions: ['txt', 'md', 'markdown'],
-                    },
-                ],
-                properties: ['openFile'],
-            })
-            .then(function (response) {
-                if (!response.canceled) {
-                    currentFile = path.basename(response.filePaths[0]);
-                    filePaths[currentFile] = response.filePaths[0];
+	.querySelector('#openfileBtn')
+	.addEventListener('click', function (event) {
+		dialog
+			.showOpenDialog({
+				title: 'Select a File to Open',
+				filters: [
+					{
+						name: 'Minecraft Function',
+						extensions: ['mcfunction'],
+					},
+					{
+						name: 'JavaScript Object Notation',
+						extensions: ['json'],
+					},
+					{
+						name: 'Text',
+						extensions: ['txt', 'md', 'markdown'],
+					},
+				],
+				properties: ['openFile'],
+			})
+			.then(function (response) {
+				if (!response.canceled) {
+					currentFile = path.basename(response.filePaths[0]);
+					filePaths[currentFile] = response.filePaths[0];
 
-                    $('.files').append(
-                        '<li onclick="openFile($(this).text());">' +
-                            currentFile +
-                            '</li>'
-                    );
+					$('.files').append(
+						'<li onclick="openFile($(this).text());">' +
+							currentFile +
+							'</li>'
+					);
 
-                    fs.readFile(filePaths[currentFile], function (err, data) {
-                        if (err) return console.log(err);
+					fs.readFile(filePaths[currentFile], function (err, data) {
+						if (err) return console.log(err);
 
-                        $('.file-content').text(data.toString());
-                    });
-                } else {
-                    console.log('no file selected');
-                }
-            });
-    });
+						$('.file-content').text(data.toString());
+					});
+				} else {
+					console.log('no file selected');
+				}
+			});
+	});
 
 // Open folder
 document
-    .querySelector('#openfolderBtn')
-    .addEventListener('click', function (event) {
-        dialog
-            .showOpenDialog({
-                title: 'Select a Folder to Open',
-                properties: ['openDirectory'],
-            })
-            .then(function (response) {
-                if (!response.canceled) {
-                    fs.readdir(response.filePaths[0], function (err, files) {
-                        if (err) return console.log(err);
+	.querySelector('#openfolderBtn')
+	.addEventListener('click', function (event) {
+		dialog
+			.showOpenDialog({
+				title: 'Select a Folder to Open',
+				properties: ['openDirectory'],
+			})
+			.then(function (response) {
+				if (!response.canceled) {
+					fs.readdir(response.filePaths[0], function (err, files) {
+						if (err) return console.log(err);
 
-                        for (let file of files) {
-                            console.log(file);
-                        }
-                    });
-                } else {
-                    console.log('no folder selected');
-                }
-            });
-    });
+						for (let file of files) {
+							console.log(file);
+						}
+					});
+				} else {
+					console.log('no folder selected');
+				}
+			});
+	});
 
 function openFile(fileName) {
-    // Open file on <li> click
+	// Open file on <li> click
 
-    currentFile = fileName;
-    filePath = filePaths[currentFile];
+	currentFile = fileName;
+	filePath = filePaths[currentFile];
 
-    fs.readFile(filePaths[currentFile], function (err, data) {
-        if (err) return console.log(err);
+	fs.readFile(filePaths[currentFile], function (err, data) {
+		if (err) return console.log(err);
 
-        $('.file-content').text(data.toString());
-    });
+		$('.file-content').text(data.toString());
+	});
 }
 
 // Save files
 function saveFile() {
-    if (currentFile != '') {
-        fs.writeFile(
-            filePaths[currentFile],
-            parseBrakes($('.file-content').html()),
-            function (err) {
-                if (err) return console.log(err);
-            }
-        );
-    } else {
-        saveFileAs();
-    }
+	if (currentFile != '') {
+		fs.writeFile(
+			filePaths[currentFile],
+			parseBrakes($('.file-content').html()),
+			function (err) {
+				if (err) return console.log(err);
+			}
+		);
+	} else {
+		saveFileAs();
+	}
 }
 
 function saveFileAs() {
-    // Save as
-    dialog
-        .showSaveDialog({
-            title: 'Select the File Path to save',
-            buttonLabel: 'Save',
-            filters: [
-                {
-                    name: 'Minecraft Function',
-                    extensions: ['mcfunction'],
-                },
-                {
-                    name: 'JavaScript Object Notation',
-                    extensions: ['json'],
-                },
-                {
-                    name: 'Minecraft Metafile',
-                    extensions: ['mcmeta'],
-                },
-                {
-                    name: 'Plain Text',
-                    extensions: ['txt'],
-                },
-                {
-                    name: 'Markdown',
-                    extensions: ['md'],
-                },
-            ],
-            properties: [],
-        })
-        .then(function (response) {
-            if (!response.canceled) {
-                currentFile = path.basename(response.filePath.toString());
-                filePaths[currentFile] = response.filePath.toString();
+	// Save as
+	dialog
+		.showSaveDialog({
+			title: 'Select the File Path to save',
+			buttonLabel: 'Save',
+			filters: [
+				{
+					name: 'Minecraft Function',
+					extensions: ['mcfunction'],
+				},
+				{
+					name: 'JavaScript Object Notation',
+					extensions: ['json'],
+				},
+				{
+					name: 'Minecraft Metafile',
+					extensions: ['mcmeta'],
+				},
+				{
+					name: 'Plain Text',
+					extensions: ['txt'],
+				},
+				{
+					name: 'Markdown',
+					extensions: ['md'],
+				},
+			],
+			properties: [],
+		})
+		.then(function (response) {
+			if (!response.canceled) {
+				currentFile = path.basename(response.filePath.toString());
+				filePaths[currentFile] = response.filePath.toString();
 
-                $('.files').append(
-                    '<li onclick="openFile($(this).text());">' +
-                        currentFile +
-                        '</li>'
-                );
+				$('.files').append(
+					'<li onclick="openFile($(this).text());">' +
+						currentFile +
+						'</li>'
+				);
 
-                fs.writeFile(
-                    filePaths[currentFile],
-                    parseBrakes($('.file-content').html()),
-                    function (err) {
-                        if (err) console.log(err);
-                    }
-                );
-            } else {
-                console.log('no file selected');
-            }
-        });
+				fs.writeFile(
+					filePaths[currentFile],
+					parseBrakes($('.file-content').html()),
+					function (err) {
+						if (err) console.log(err);
+					}
+				);
+			} else {
+				console.log('no file selected');
+			}
+		});
 }
