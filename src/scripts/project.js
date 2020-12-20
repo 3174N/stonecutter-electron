4f8a9414150b856d08ab9caabbbe9abc865b2ae8
3174N <eitan.levy.159@gmail.com>
2020-12-20 14:00:36 +0200

    Add popup window

---
+++ src/scripts/project.js
@@ -0,0 +1,34 @@
+const remote = require('electron').remote;
+const { BrowserWindow } = remote;
+const url = require('url');
+
+let win;
+
+function openPopup() {
+    win = new BrowserWindow({
+        width: 500,
+        height: 350,
+        icon: path.join(__dirname, 'styles/media/icon.ico'),
+        webPreferences: {
+            nodeIntegration: true,
+            enableRemoteModule: true,
+        },
+    });
+    win.loadURL(
+        url.format({
+            pathname: path.join(__dirname, '/pages/popup.html'),
+            protocol: 'file:',
+            slashes: true,
+        })
+    );
+    // * Comment next line for dev-tools access * //
+    win.removeMenu();
+}
+
+function closePopup() {
+    remote.getCurrentWindow().close();
+}
+
+document.querySelector('#closeBtn').addEventListener('click', function (event) {
+    closePopup();
+});
