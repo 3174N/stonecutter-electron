5a82ea0fc540d96cbd7e4fcdd07fb784a20454d6
SFR <664ecb67-c3b4-4f09-854a-f4535569b492@sfr.anonaddy.com>
2020-12-23 20:56:22 +0200

    Add AppData support

--- src/scripts/settings.js
+++ src/scripts/settings.js
@@ -2,8 +2,21 @@ var fs = require('fs');
 var path = require('path');
 
 function initSettings() {
-    // TODO: OS-dependent file location
-    var settingsFolder = path.join(process.env.HOME, '.config/stonecutter');
+    switch (process.platform) {
+        case 'linux':
+        case 'darwin':
+            var settingsFolder = path.join(
+                process.env.HOME,
+                '.config/stonecutter',
+            );
+            break;
+        case 'win32':
+            var settingsFolder = path.join(
+                process.env.HOME,
+                'AppData\\Roaming\\stonecutter',
+            );
+            break;
+    }
 
     if (!fs.existsSync(path.join(settingsFolder, 'settings.cutter'))) {
         console.log('File settings.cutter not found. Creating...');
