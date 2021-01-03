8777c0951a27c787ad2ebe44a7d4dee3aca74532
3174N <eitan.levy.159@gmail.com>
2021-01-03 14:28:56 +0200

    Fix project in titlebar

--- src/scripts/project.js
+++ src/scripts/project.js
@@ -140,7 +140,8 @@ function findProject(file) {
 }
 
 ipc.on('get-project', (event, file) => {
-    return findProject(file);
+    global.projectName = 'Test';
+    // global.projectName = findProject(file);
 });
 
 document.querySelector('#dirBtn').addEventListener('click', (event) => {
