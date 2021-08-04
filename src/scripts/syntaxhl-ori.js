29eb37683616acfc82fa948a1235718f82049c1e
g33king <turhuhxckuo@gmail.com>
2021-08-04 14:21:21 +0300

    Add explanations

--- src/scripts/syntaxhl-ori.js
+++ src/scripts/syntaxhl-ori.js
@@ -11,7 +11,7 @@ function textUpdate(e) {
     if (e.code != "Space") {
         return;
     }
-
+    // saving the start and end point of the selection before messing it up
     let range, start, end;
     if (typeof window.getSelection != "undefined") {
         range = window.getSelection().getRangeAt(0).cloneRange();
@@ -19,20 +19,22 @@ function textUpdate(e) {
         end = range.endOffset;
         console.log(start, end);
     }
-
+    
+    // messing it up and marking the text need to be marked
     let words = txtbox.text().split(" ");
     let html = "";
     for (let i = 0; i < words.length; i++) {
-        if (["execute", "kill"].indexOf(words[i]) != -1) {
-            html += "<mark>" + words[i] + "</mark> ";
+        if (["execute", "kill"].indexOf(words[i]) != -1) { // change the list here to the list of the commands
+            html += "<mark>" + words[i] + "</mark> "; // change the <mark> tags to change the style and stuff
         }
         else if (words[i] != "") {
             html += words[i] + " ";
         }
     }
-    
+    // update html
     txtbox.html(html);
-
+    
+    // unmessing the selection
     if (typeof window.getSelection != undefined) {
         range = window.getSelection().getRangeAt(0);
         range.setStart(range.startContainer.lastChild, range.endContainer.lastChild.length);
